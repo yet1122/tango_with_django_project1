baf988f0ce5b8be154d758c832349d02baa625bc
yet1122 <41500703+yet1122@users.noreply.github.com>
2019-12-30 12:25:09 +0800

    Update settings.py

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -33,7 +33,7 @@ with open('secret.key') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['rangodemo2019.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['zyt.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
